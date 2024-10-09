0b15d143cefa85f1290716e2babda7e7c4eab48e
dipatpat <patrycja.szpakowska@gmail.com>
2024-10-09 07:28:00 +0200

    add Adder class

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@
 public class Main {
         public static void main(String[] args) {
             Adder adder = new Adder();
-            System.out.println(adder.add(1, 2));
+            System.out.println(Adder.add(1, 2));
             Subtractor subtractor = new Subtractor();
             System.out.println(subtractor.subtract(6, 3));
 
